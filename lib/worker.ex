800882413656eab7772245d51ab36d18dd6c4ad2
Jonathan Salis <jon@voxpoplabs.com>
2017-09-19 08:33:02 -0400

    Simplifying worker call

--- lib/worker.ex
+++ lib/worker.ex
@@ -5,26 +5,13 @@ defmodule MongoErrorTest.Worker do
     GenServer.start_link(__MODULE__, nil, [])
   end
 
-  def find(pid, collection, query) do
-    GenServer.call(pid, {:find, collection, query})
+  def init(_state) do
+    database = Application.get_env(:mongo_error_test, :database)
+    Mongo.start_link(database: database)
   end
 
-  @doc """
-  Called the first time before a connection is established
-  Set up a new connection and query mongo
-  """
-  def handle_call({:find, collection, query}, _from, nil) do
-    database = Application.get_env(:mongo_error_test, :database)
-    {:ok, conn} = Mongo.start_link(database: database)
-
-    # Uncomment below to wait for mongo conn to become available
-    # Process.sleep(100)
-    
-    IO.inspect(conn, label: "Starting worker")
-
-    results = Mongo.find_one(conn, collection, query)
-
-    {:reply, results, conn}
+  def find(pid, collection, query) do
+    GenServer.call(pid, {:find, collection, query})
   end
 
   @doc """
